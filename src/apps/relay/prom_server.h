b799d012ac219468ad0e6e7b0ea14f1b8b19061b
Iñaqui <inaqui@signal.org>
2023-05-05 11:40:48 -0400

    duration and rate counter metric

--- src/apps/relay/prom_server.h
+++ src/apps/relay/prom_server.h
@@ -66,7 +66,10 @@ void prom_set_finished_traffic(const char *realm, const char *user, unsigned lon
                                unsigned long sentp, unsigned long sentb, bool peer);
 
 void prom_inc_allocation(SOCKET_TYPE type);
-void prom_dec_allocation(SOCKET_TYPE type);
+void prom_dec_allocation(SOCKET_TYPE type,
+                         unsigned long duration,
+                         unsigned long received_rate_kbps,
+                         unsigned long sent_rate_kbps);
 
 void prom_inc_stun_binding_request(void);
 void prom_inc_stun_binding_response(void);
